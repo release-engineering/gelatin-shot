d76fd043c8c7b9c6ea6ddcd2f3cefb15c2d8fa23
Cloud User <cloud-user@aheeney-gelatin-shot.os1.phx2.redhat.com>
2015-07-13 13:12:51 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='gelatin-shot',
+      version = '0.1',
       packages=find_packages(),
       scripts=['bin/gelatinshot'],
       install_requires=[
-          'Flask'
+          'Flask',
+          'SQLAlchemy',
+          'simplejson',
+          'psycopg2',
 #          'kerberos>=1.1.1',
 #          'Flask-Kerberos',
-          ]
+          ],
      )

12627886d97aca5f0f83b647faf213c6a1aa21d1
Cloud User <cloud-user@aheeney-gelatin-shot.os1.phx2.redhat.com>
2015-07-13 09:23:18 -0400

    Removed references to kerberos based things

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gelatin-shot',
-      packages=find_packages()
+      packages=find_packages(),
+      scripts=['bin/gelatinshot'],
+      install_requires=[
+          'Flask'
+#          'kerberos>=1.1.1',
+#          'Flask-Kerberos',
+          ]
      )

5a71ab2eecfd590ba1eb8b441c5efb7ffd82b8e7
Cloud User <angelica.heeney@gmail.com>
2015-07-31 13:52:06 -0400

    Gelatinshot is a package/module now, not a script. It works with gunicorn now

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name='gelatin-shot',
+setup(name='gelatinshot',
       version = '0.1',
-      packages=find_packages(),
-      scripts=['bin/gelatinshot'],
+      packages=['gelatinshot'],
+      package_dir={'gelatinshot': 'bin'},
+      py_modules = ['bin/gelatinshot'],
       install_requires=[
           'Flask',
           'SQLAlchemy',
           'simplejson',
           'psycopg2',
-#          'kerberos>=1.1.1',
-#          'Flask-Kerberos',
+          'kerberos==1.1.1',
+          'Flask-Kerberos',
+          'requests-kerberos'
           ],
      )
